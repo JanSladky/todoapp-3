fd47a647ed038daf715c4248fede42178395be71
Jan Sladký <sladky.honza@gmail.com>
2024-02-21 16:42:06 +0100

    Pridani funkce pro pridani noveho itemu do seznamu pri zmacknuti enter

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div><input type="text" class="todo-input" placeholder="Zadej úkol" v-model="newTodo" /></div>
+  <div><input type="text" class="todo-input" placeholder="Zadej úkol" v-model="newTodo" @keyup.enter="addTodo"></div>
   <div v-for="todo in todos" :key="todo.id" class="todo-item">
     {{todo.title}}
 
@@ -12,6 +12,7 @@ export default {
   data() {
     return {
       newTodo: "",
+      idForTodo: 3,
       todos: [
         {
           'id': 1,
@@ -32,6 +33,18 @@ export default {
       ],
     };
   },
+  methods: {
+    addTodo() {
+        this.todos.push({
+            id: this.idForTodo,
+            title: this.newTodo,
+            completed: false,
+
+        })
+        this.newTodo = ''
+        this.idForTodo++
+    }
+  }
 };
 </script>
 
